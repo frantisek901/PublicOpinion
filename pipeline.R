8cca158fa249e0af89cc55b2873a82c956e06694
František Kalvas <kalvas@kss.zcu.cz>
2022-01-23 02:02:15 +0100

    Clear version of pipeline!

--- pipeline.R
+++ pipeline.R
@@ -1,7 +1,7 @@
 #### Script for Project Group '#public-opinion-project' from SFI-CI Winter 2022
 ##
 ##   This script handles getting data from NetLogo model,
-##   now named: public_opinion_V01.nlogo
+##   now named: public_opinion_v4pipeline.nlogo
 ##   into R and storing them t/here for further network analyses.
 ##
 ##   NOTES:
@@ -11,9 +11,11 @@
 ##   2) dataframe/tibble with agents' traits and variables
 ##   This leads to flexible, but stil lightweight datastructure allowing us in case of need store info on every tick of simmulation.
 ##
+##   B) Current state of data stored is even better -- whole experiment is stored in one object/file!
+##
 
 ## Encoding: windows-1250
-## Last edit: 2022-01-22 Fran»esko
+## Last edit: 2022-01-23 Fran»esko
 ##
 
 
@@ -23,16 +25,16 @@ rm(list = ls())
 
 # Packages
 library(nlrx)
-library(RNetLogo)
 library(dplyr)
 library(tibble)
+library(igraph)
 library(ggplot2)
 
 
+#### Controling NetLogo from R with 'nlrx' package  
 
-# Controling NetLogo from R: The first tries with nlrx ------------------------------
 
-## Creation of `nl` object:
+# Creation of `nl` object: ---------------------------------------------
 
 # Windows default NetLogo installation path (adjust to your needs!):
 netlogopath = "c:/Program Files/NetLogo 6.2.2/"
@@ -40,17 +42,20 @@ netlogopath = "c:/Program Files/NetLogo 6.2.2/"
 #  Does anyone know the way how to find the path to our own NetLogo instalations without setting it manually?
 #  I am afraid that this manual setting forces many people avoid good things -- many ncluding me...
 
-modelpath = "public_opinion_v01.nlogo"
+modelpath = "public_opinion_v4pipeline.nlogo"
 
-outpath = paste0(getwd(),"/OutputData")
+outpath = paste0(getwd(),"/OutputData")  
+# NOTE: Git doesn't cover this folder, do not forget create it manually in main project folder.
 
 nl = nl(nlversion = "6.2.2",
         nlpath = netlogopath,
         modelpath = modelpath,
-        jvmmem = 2048)
+        jvmmem = 3072)
 
 
-## Attaching an experiment:
+
+# Attaching an experiment: ------------------------------------------------
+
 nl@experiment = experiment(expname="firstTry",
                             outpath=outpath,
                             repetition=1,
@@ -59,7 +64,7 @@ nl@experiment = experiment(expname="firstTry",
                             idgo="go",
                             runtime=50,
                             evalticks=0:50,
-                            metrics = c("count turtles", "count turtles with [opinion < 50]", "count links"),
+                            metrics = c("count turtles", "count turtles with [opinion < 50]", "count links"),  # NOTE: We might create here reporters which do not exist in the simulation!
                             metrics.turtles = list("turtles" = c("who", "opinion", "tolerance", "num-family-ties", "num-coworker-ties", "num-friend-ties")),
                             metrics.links = list(
                               "family" = c("[who] of end1", "[who] of end2", "weight"),
@@ -70,54 +75,70 @@ nl@experiment = experiment(expname="firstTry",
                             constants = list("transparency" = 255))
 
 
-## Attaching a simulation design:
+
+# Attaching a simulation design: ------------------------------------------
+
 nl@simdesign = simdesign_ff(nl=nl, nseeds=3)
 
+
+
+# Producing results ----------------------------------
 
 ## Running a single simulation:
 a = Sys.time()
 results = run_nl_one(nl = nl, seed = getsim(nl, "simseeds")[1], siminputrow = 3)
 Sys.time() - a
+## Computational time on Fran»esko's computer: 14.6 sec.
 
 
-## Running an experiment:
+## Running whole experiment:
 a = Sys.time()
 results = run_nl_all(nl = nl)
 Sys.time() - a
-results
+## Computational time on Fran»esko's computer: 5.8 min. => 13 sec. per single simulation!
 
 
-## Attaching results to `nl` object:
+
+# Attaching results to `nl` object: ---------------------------------------
+
 setsim(nl, "simoutput") = results
-write_simoutput(nl)  # Writing output to the output folder.
-analyze_nl(nl)  # Further analysis.
-eval_simoutput(nl)  # Kinda evaluation.
 
 
-## Something with network...
+
+# Saving object 'nl' with whole experiment: -------------------------------
+
+save(nl, file = "wholeExperiment.RData")
+
+
+
+# Create object with network data and visualize them --------
+
+# One object for all network data pulled from 'nl':
 nl.graph = nl_to_graph(nl)
 
-# Tick 0
-nl.graph.0 = nl.graph$spatial.links[[1]]
-V(nl.graph.0)
-E(nl.graph.0)
-plot.igraph(nl.graph.0, vertex.size=8, vertex.label=NA, edge.arrow.size=0.2)
+# Tick 0 of the first simulation:
+sim01.tick00 = nl.graph$spatial.links[[1]]
+V(sim01.tick00)
+E(sim01.tick00)
+plot.igraph(sim01.tick00, vertex.size=8, vertex.label=NA, edge.arrow.size=0.2)
 
-# Tick 250
-nl.graph.250 = nl.graph$spatial.links[[2]]
-V(nl.graph.250)
-E(nl.graph.250)
-plot.igraph(nl.graph.250, vertex.size=8, vertex.label=NA, edge.arrow.size=0.2)
+# Tick 0 of the third simulation:
+sim03.tick00 = nl.graph$spatial.links[[103]]
+V(sim03.tick00)
+E(sim03.tick00)
+plot.igraph(sim03.tick00, vertex.size=8, vertex.label=NA, edge.arrow.size=0.2)
 
-# Tick 500
-nl.graph.500 = nl.graph$spatial.links[[3]]
-V(nl.graph.500)
-E(nl.graph.500)
-plot.igraph(nl.graph.500, vertex.size=8, vertex.label=NA, edge.arrow.size=0.2)
+# Tick 50 of the third simulation:
+sim03.tick50 = nl.graph$spatial.links[[153]]
+V(sim03.tick50)
+E(sim03.tick50)
+plot.igraph(sim03.tick50, vertex.size=8, vertex.label=NA, edge.arrow.size=0.2)
 
 
-## Let's draw some graph:
-df = results %>% filter(`[step]` == 0) %>% select(7:9)
+
+# Some graphs on agent variables: -----------------------------------------
+
+df = nl@simdesign@simoutput %>% filter(`[step]` == 0) %>% select(7:9)
 names(df) = c("Agents", "Negatives", "Links")
 
 ggplot(df, aes(x = Agents, y = Negatives)) +
