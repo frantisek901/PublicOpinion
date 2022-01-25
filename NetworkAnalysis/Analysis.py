3c12dd96ad9754f88cf40d09b0194b2f93e115d2
Betancourt Valencia <jose.betancourtvalencia@yale.edu>
2022-01-25 18:15:27 -0500

    modified:   NetworkAnalysis/Analysis.py

--- NetworkAnalysis/Analysis.py
+++ NetworkAnalysis/Analysis.py
@@ -19,3 +19,12 @@ contributors:
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+#------------------------------------------------------------------------------
+# IMPORT AND PROCESS DATA
+#------------------------------------------------------------------------------
+fname = '../public_opinion_v01_crazy_links output_test_3-ties-adj-matrices-and-opinions-table.csv'
+names = ['run', 'n_agents', 'tolerance', 'transparency', 'adj_or_not', 
+         'step', 'fam_adj', 'work_adj', 'friend_adj', 'opinions']
+
+df = pd.read_csv(fname, sep=',', skiprows=7, names=names)
