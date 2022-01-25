a66c0c9821703cf9462e2e79ea5960afe59d89a7
Betancourt Valencia <jose.betancourtvalencia@yale.edu>
2022-01-25 17:56:03 -0500

    new file:   NetworkAnalysis/Analysis.py

---
+++ NetworkAnalysis/Analysis.py
@@ -0,0 +1,21 @@
+"""
+    This script calculates distributional and network measures for the public
+    opinion ABM.
+-------------------------------------------------------------------------------
+created on:
+    Tue 25 Jan 2022
+-------------------------------------------------------------------------------
+last change:
+    Tue 25 Jan 2022
+-------------------------------------------------------------------------------
+notes:
+-------------------------------------------------------------------------------
+contributors:
+    Jose:
+        name:       Jose Betancourt
+        email:      jose.betancourtvalencia@yale.edu
+-------------------------------------------------------------------------------
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
